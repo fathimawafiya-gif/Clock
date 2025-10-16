83e4789bfab8cf3b723a53b0007e10892c6bbca4
fathimawafiya-gif <fathimawafiya2006@gmail.com>
2025-10-16 11:55:17 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,19 @@
+function setDate() {
+    const now = new Date();
+
+    const seconds = now.getSeconds();
+    const secondsDegrees = ((seconds / 60) * 360) + 90;
+    document.querySelector('.second-hand').style.transform = `rotate(${secondsDegrees}deg)`;
+
+    const minutes = now.getMinutes();
+    const minutesDegrees = ((minutes / 60) * 360) + ((seconds/60)*6) + 90;
+    document.querySelector('.minute-hand').style.transform = `rotate(${minutesDegrees}deg)`;
+
+    const hours = now.getHours();
+    const hoursDegrees = ((hours / 12) * 360) + ((minutes/60)*30) + 90;
+    document.querySelector('.hour-hand').style.transform = `rotate(${hoursDegrees}deg)`;
+}
+
+setInterval(setDate, 1000);
+
+setDate();
